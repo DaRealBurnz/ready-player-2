6503c184f15455eabbc800ae09f86b7e4a13c6c7
DaRealBurnz <87015034+DaRealBurnz@users.noreply.github.com>
2021-07-15 12:57:52 +1000

    Add files via upload
    Sign up done

--- hackathon/add data.js
+++ hackathon/add data.js
@@ -1,13 +1,18 @@
 function new_usr() {
-    if ($("#password") !== $("#conf_password")) {
-        $("label")[5].css("color", "red")
+    success = true;
+    $("label").css("color","black");
+    if ($("#password").val() !== $("#conf_password").val()) {
+        $("label#5").css("color", "red");
+        success = false;
     }
     $.ajax({
         type: "POST",
         url: "add.php",
         data: {fname:$("#fname").val(), lname:$("#lname").val(), email:$("#email").val(), discord:$("#discord").val(), password:$("#password").val()},
         success: function(res) {
-            if (res == "Done") {
+            //$('body').append(res);
+            if (res === "Done" && success === true) {
+                $('body').append(res);
                 var form = $('<form action="login_page.php" method="post" style="display:none">' +
                     '<input type="text" name="email" value="' + $("#email").val() + '" />' +
                     '<input type="text" name="password" value="' + $("#password").val() + '" />' +
@@ -15,9 +20,10 @@ function new_usr() {
                 $('body').append(form);
                 form.submit();
             } else {
-                errors = res.split(",")
+                errors = res.split(",");
+                errors.pop();
                 errors.forEach(function(value, index, array) {
-                    $("label")[value].css("color","red")
+                    $("label#"+value).css("color","red");
                 })
             }
         }
--- hackathon/add.php
+++ hackathon/add.php
@@ -1,3 +1,36 @@
 <?php
 $dbc = mysqli_connect('localhost', 'root', 'password', 'hackathon_testing');
+//if (!$dbc) exit('Could not connect');
+$errors = array();
+if (empty($_POST['fname'])) {
+    array_push($errors, '0');
+}
+if (empty($_POST['lname'])) {
+    array_push($errors, '1');
+}
+if (empty($_POST['email'])) {
+    array_push($errors, '2');
+}
+if (empty($_POST['discord'])) {
+    array_push($errors, '3');
+}
+if (empty($_POST['password'])) {
+    array_push($errors, '4');
+}
+if (empty($errors)) {
+    $stmt = mysqli_prepare($dbc, "INSERT INTO users (`first_name`, `last_name`, `email`, `discord`, `password`) VALUES (?, ?, ?, ?, ?)");
+    mysqli_stmt_bind_param($stmt, 'sssss', $_POST['fname'], $_POST['lname'], $_POST['email'], $_POST['discord'], $_POST['password']);
+    $success = mysqli_stmt_execute($stmt);
+    /*if ($success) {
+        echo "yes";
+    } else {
+        echo "no";
+    }
+    echo gettype($_POST['discord']);*/
+    echo "Done";
+} else {
+    foreach ($errors as $value) {
+        echo "$value,";
+    }
+}
 ?>
