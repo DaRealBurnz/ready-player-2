ece19f9db92a18655d984b8101bee4fb06318182
DaRealBurnz <87015034+DaRealBurnz@users.noreply.github.com>
2021-07-16 01:34:11 +1000

    Add files via upload

--- hackathon/verify_user_function.php
+++ hackathon/verify_user_function.php
@@ -1,18 +1,7 @@
 <?php
-function verify_login($con) {
-    if (isset($_SESSION['user_id'])){
-        $id = $_SESSION['user_id'];
-        $query = "select * from users where user_id = '$id' limit 1 ";
-
-        $result = mysqli_query($con, $query);
-        if ($result && mysqli_num_rows($result) > 0){
-            $user_data = mysqli_fetch_assoc($result);
-            return $user_data;
-        }
-    }
-
-    header("Location: ###"); //Place login redirectory file if user_id does not exist
-    die();
-
+session_start();
+if (!isset($_SESSION['user_id'])){
+    echo "Not logged in! Redirecting...";
+    header("Refresh:5; URL=login_page.php"); //Place login redirectory file if user_id does not exist
 }
 ?>
